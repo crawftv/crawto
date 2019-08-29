60d15e3cb8197f289df7c9f61049588faf555e5e
Crawford <crawford.collins@gmail.com>
2019-08-28 21:15:45 -0500

    simple imputer added

--- CrawtoML.py
+++ CrawtoML.py
@@ -2,6 +2,7 @@ import seaborn
 import matplotlib.pyplot as plt
 import numpy as np
 import re
+from math import ceil
 import pandas
 from scipy.stats import shapiro
 from sklearn.utils.multiclass import unique_labels
@@ -10,6 +11,7 @@ from sklearn.impute import IterativeImputer
 from scipy.stats import probplot
 from sklearn.preprocessing import LabelEncoder
 from category_encoders.target_encoder import TargetEncoder
+from sklearn.impute import SimpleImputer
 
 
 class CrawtoML:
@@ -17,7 +19,7 @@ class CrawtoML:
     """
 
     def __init__(
-        self, data, target, features="infer", problem="infer", imputer="most_frequent"
+        self, data, target, features="infer", problem="infer", 
     ):
         self.data = data
         self.target = target
@@ -25,7 +27,8 @@ class CrawtoML:
         self.categorical_features = []
         self.problematic_columns = []
         self.numeric_features = []
-        self.imputed_columns = []
+        self.label_encoded_features = []
+        self.imputed_features = []
         if features == "infer":
             self.features = list(self.data.columns)
             self.features.remove(self.target)
@@ -58,41 +61,44 @@ class CrawtoML:
     def target_encoder(self):
         self.target_encoder = TargetEncoder()
         self.target_encoder
-        pass
 
     def define_nan_features(self):
-        df = pandas.DataFrame(self.data.isna().sum())
-        for i in self.features:
-            if df.loc[i][0] > 0:
+        l = self.features
+        to_remove = []
+        for i in l:
+            if self.data[i].isna().sum() > 0:
                 self.nan_features.append(i)
-                self.features.remove(i)
+                to_remove.append(i)
+        for j in to_remove:
+            self.features.remove(j)
 
-    def imputation(self):
-        # http://www.stat.columbia.edu/~gelman/arm/missing.pandasf
-        # http://www.feat.engineering/imputation-methods.html#fnref74
+    def encode_nan(self):
         for i in self.nan_features:
             if self.data[i].dtype == "O":
-                self.data[i] = self.data[i].astype("str")
                 le = LabelEncoder()
-                le.fit(self.data[i])
-                t = le.transform(self.data[i])
-                imputer = IterativeImputer()
-                self.data[f"imputed_{i}"] = imputer.fit_transform(
-                    imputer.fit_transform(t.reshape(-1, 1))
-                )
-                self.imputed_columns.append(i)
-                self.nan_features.remove(i)
-            elif self.data[i].dtype == 'float64':
-                imputer = IterativeImputer()
-                self.data[f"imputed_{i}"] = imputer.fit_transform(
-                    imputer.fit_transform(np.array(self.data[i]).reshape(-1, 1))
-                )
-                self.imputed_columns.append(i)
-                self.nan_features.remove(i)
+                t = le.fit_transform(self.data[i].values.reshape(-1, 1))
+                self.label_encoded_features.append(i)
+                self.data["le_{i}"] = t
 
+    def imputation(self):
+        l = self.nan_features
+        for i in l:
+            if self.data[i].dtype == "O":
+                si = SimpleImputer(strategy="most_frequent")
+                t = si.fit_transform(self.data[i].values.reshape(-1, 1))
+                self.data[f"imputed_{i}"] = t
+                self.imputed_features.append(f'imputed_{i}')
+                self.categorical_features.append(f"imputed_{i}")
+            elif self.data[i].dtype == "float64":
+                si = SimpleImputer(strategy="median")
+                t = si.fit_transform(self.data[i].values.reshape(-1,1))
+                self.data[f"imputed_{i}"] = t
+                self.imputed_features.append(f'imputed_{i}')
+                self.numeric_features.append(f"imputed_{i}")
 
+    
     def other_types(self):
-        others = [i for i in self.features if i not in self.numeric_columns]
+        others = [i for i in self.features if i not in self.numeric_features]
         self.other_types = others
         return self.other_types
 
@@ -117,7 +123,7 @@ class CrawtoML:
 
     #         print(
     #             seaborn.PairGrid(
-    #                 self.data, x_vars=self.numeric_columns, y_vars=self.target
+    #                 self.data, x_vars=self.numeric_features, y_vars=self.target
     #             ).map(seaborn.distplot)
     #         )
 
@@ -126,9 +132,9 @@ class CrawtoML:
             pandas.DataFrame(
                 [
                     self.distribution_fit(self.data, i)
-                    for i in self.numeric_columns + [self.target]
+                    for i in self.numeric_features + [self.target]
                 ],
-                index=self.numeric_columns + [self.target],
+                index=self.numeric_features + [self.target],
             )
         )
 
@@ -172,13 +178,12 @@ class CrawtoML:
             print("No Features are correlated above the threshold")
 
     def probability_plots(self):
-        len_plots = len(self.numeric_features) / 2
-        fig = plt.figure(figsize=(12, 12))
+        len_plots = ceil(len(self.numeric_features) / 2)
+        fig = plt.figure(figsize=(12, len_plots*3))
         fig.tight_layout()
         for i in range(1, len(self.numeric_features) + 1):
             fig.add_subplot(len_plots, 2, i)
-
-            probplot(automl.data[self.numeric_features[i - 1]], plot=plt)
+            probplot(self.data[self.numeric_features[i-1]], plot=plt)
             plt.subplots_adjust(
                 left=None, bottom=None, right=None, top=None, wspace=0.35, hspace=0.35
             )
@@ -190,11 +195,11 @@ class CrawtoML:
         Problematic Columns: {self.problematic_columns}\n\
         Feature Columns: {self.categorical_features}\n\
         Numeric Columns: {self.numeric_features}\n\
-        Imputed Columns: {self.imputed_columns}\n\
+        Imputed Columns: {self.imputed_features}\n\
         NAN Columns:     {self.nan_features}"
 
 
 #            """Saved patterns"""
-#  print(seaborn.PairGrid(self.data, x_vars=self.numeric_columns, y_vars=self.target).map(
+#  print(seaborn.PairGrid(self.data, x_vars=self.numeric_features, y_vars=self.target).map(
 #             seaborn.boxplot
 #         ))
