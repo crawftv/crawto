2946284ee6724e931ad9d3c7c5bd938947c419c8
Crawford <crawford.collins@gmail.com>
2019-09-24 23:53:00 -0500

    tests are successful for charts.py

--- tests/test_charts.py
+++ tests/test_charts.py
@@ -1,16 +1,20 @@
 import pytest
-from crawto.charts import tsne, make_html
-from hypothesis import given
-from hypothesis.strategies import lists
+from crawto.charts import *
+import IPython
+from IPython.display import display, HTML
 
-
-@given(list())
-@example()
-def test_make_html():
-    assert type(tsne( )) ==
+#label_list = ["a", "b", "c"]
+#x_list = [-1, 0, 1]
+#y_list = [1, 1, 1]
 
 
 def test_make_html():
-    assert make_html( ) == html
+    label_list = ["a", "b", "c"]
+    x_list = [-1, 0, 1]
+    y_list = [1, 1, 1]
+    assert type(tsne(label_list, x_list, y_list)) is IPython.core.display.HTML
 
 
+#def test_make_html():
+#    
+#    assert make_html( ) == html
