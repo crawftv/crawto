74865bd4ec084fd048bfbfc7edd207c50854d1a6
Crawford <crawford.collins@gmail.com>
2019-09-28 01:08:28 -0500

    blackened test files, started building tests for CrawtoDS.py

--- tests/test_charts.py
+++ tests/test_charts.py
@@ -7,27 +7,30 @@ label_list = ["a", "b", "c"]
 x_list = [-1, 0, 1]
 y_list = [1, 1, 1]
 
+
 def test_convert_lists_to_chartsjs_data():
     label_list = ["a", "b", "c"]
     x_list = [-1, 0, 1]
     y_list = [1, 1, 1]
-    
-    datasets,label_list = convert_lists_to_chartsjs_data(label_list,x_list,y_list)
-    assert type(datasets) is list 
+
+    datasets, label_list = convert_lists_to_chartsjs_data(label_list, x_list, y_list)
+    assert type(datasets) is list
     assert type(label_list) is list
+
+
 #    assert datasets.keys() ==['x','y']
 #    assert datasets.
 
-#def test_make_html():
+# def test_make_html():
 #    label_list = ["a", "b", "c"]
 #    x_list = [-1, 0, 1]
 #    y_list = [1, 1, 1]
 #
 #
-#    
+#
 #    assert type(tsne(label_list, x_list, y_list)) is IPython.core.display.HTML
 
 
-#def test_make_html():
-#    
+# def test_make_html():
+#
 #    assert make_html( ) == html
