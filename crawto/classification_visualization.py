7486b00be1a678ce49448f5f125535bf471a0141
Crawford <crawford.collins@gmail.com>
2019-09-30 20:21:58 -0500

    downgraded matplotlib to work with heatmap betteer. added roc and prc plots to classification visualization. baseline predictions and simple logistic regresssions are ready

--- crawto/classification_visualization.py
+++ crawto/classification_visualization.py
@@ -1,6 +1,7 @@
 import sklearn
 from sklearn import metrics
 from sklearn.metrics import classification_report
+from sklearn.metrics import precision_recall_curve, average_precision_score, roc_curve, auc, roc_auc_score
 from sklearn.metrics import confusion_matrix, precision_recall_curve
 from sklearn.utils.multiclass import unique_labels
 import seaborn as sns
@@ -16,13 +17,14 @@ def classification_visualization(y_true,y_pred):
     """
     Prints the results of the functions. That's it
     """
-    fig = plt.figure(figsize=(12,6))
+    fig = plt.figure()
     fig.tight_layout()
     label1 =classification_report(y_true,y_pred)
-    print(label1)
-    plt.figure(1)
+    #fig.add_subplot(3,1,1)
     confusion_viz(y_true,y_pred)
-    plt.figure(2)
+    #fig.add_subplot(3,1,2)
+    plt_roc(y_true,y_pred)
+    #fig.add_subplot(3,1,3)
     plt_prc(y_true,y_pred)
     
     plt.show();
@@ -39,7 +41,7 @@ def confusion_viz(y_true, y_pred):
     matrix = confusion_matrix(y_true, y_pred)
     graph = sns.heatmap(matrix, annot=True,
                        fmt=',', linewidths=1,linecolor='grey',
-                       square=True,
+                       square=False,
                        xticklabels=["Predicted\n" + str(i) for i in labels],
                        yticklabels=["Actual\n" + str(i) for i in labels],
                        robust=True,
@@ -48,9 +50,10 @@ def confusion_viz(y_true, y_pred):
     plt.xticks(rotation=0)
     return graph
 
-
 def plt_prc(y_true, y_pred):
+    aps = round(average_precision_score(y_true,y_pred)*100,2)
     a,b,c  = precision_recall_curve(y_true,y_pred)
+    d,e,f = roc_curve(y_true,y_pred)
     plt.figure()
     lw = 2
     plt.plot(a, b, color='darkorange',
@@ -60,6 +63,22 @@ def plt_prc(y_true, y_pred):
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic example')
+    plt.title(f'Precision Recall Curve\nAverage Precision Score = {aps}%')
     plt.legend(loc="lower right")
-    return plt
+    plt.show()
+    
+def plt_roc(y_true,y_pred):
+    plt.figure()
+    lw = 2
+    fpr, tpr, threshold = roc_curve(y_true,y_pred)
+    ras = round(roc_auc_score(y_true,y_pred)*100,2)
+    plt.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve ')
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title(f'ROC AUC Curve\nROC AUC Score = {ras}%')
+    plt.legend(loc="lower right")
+    plt.show()
