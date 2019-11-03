aba9d823994522430c99dd9eb646578f0a389430
Crawford <crawford.collins@gmail.com>
2019-11-03 12:42:02 -0600

    trying to make DRY class for Chart testing

--- tests/test_charts.py
+++ tests/test_charts.py
@@ -1,9 +1,10 @@
 import pytest
 import IPython
 from IPython.display import display, HTML
-from crawto.charts.charts import Plot, ScatterChart, BarChart, LineChart
+from crawto.charts.charts import Plot, ScatterChart, BarChart, LineChart, Chart_type
 from hypothesis import given
 from hypothesis.strategies import text, builds, iterables, floats, one_of, integers,composite
+
 #Simple Test
 x=[-1,0,1]
 y = [1,-1,1]
@@ -25,25 +26,70 @@ b.add_DataSet("t",x,y)
 b.add_DataSet("t1" ,x,[2,2,2])
 b.edit_title("BarChart")
 
-l2 = LineChart()
 
-#@given(label = text(),
-#        x=iterables(one_of(integers(),floats())),
-#        y=iterables(one_of(integers(), floats())),
-#        t = text())
-@composite
-def generate_LineChart_data(draw):
-    n = draw(integers(0,5000))
-    label = text()
-    x=iterables(one_of(integers(),floats()),min_size=n,max_size=n)
-    y=iterables(one_of(integers(),floats()),min_size=n,max_size=n)
-    t = text()
-    return draw(label),draw(x),draw(y),draw(t)
-@given(generate_LineChart_data())
-def test_LineChart(g):
-    label,x,y,t = g
-    l2.add_DataSet(label,x,y)
-    l2.edit_title(t)
-    l2.edit_xAxes(t)
-    l2.edit_yAxes(t,"linear")
-
+class TestChart:
+    def __init__(self,chart:Chart_type):
+        self.chart = chart
+    @composite
+    def generate_good_Chart_data(draw):
+        n = draw(integers(0,50))
+        label = text()
+        x=iterables(one_of(integers(),floats()),min_size=n,max_size=n)
+        y=iterables(one_of(integers(),floats()),min_size=n,max_size=n)
+        t = text()
+        return draw(label),draw(x),draw(y),draw(t)
+    @given(generate_good_Chart_data())
+    def test_Chart(g):
+        c = self.chart
+        label,x,y,t = g
+        c.add_DataSet(label,x,y)
+        c.edit_title(t)
+        c.edit_xAxes(t)
+        c.edit_yAxes(t,"linear")
+        assert c.title["text"]==t
+        assert c.xAxes[0]["scaleLabel"]["labelString"] == t
+        assert c.yAxes[0]["scaleLabel"]["labelString"] == t
+test_LineChart = TestChart(LineChart())
+#class TestLineChart:
+#    @composite
+#    def generate_good_LineChart_data(draw):
+#        n = draw(integers(0,50))
+#        label = text()
+#        x=iterables(one_of(integers(),floats()),min_size=n,max_size=n)
+#        y=iterables(one_of(integers(),floats()),min_size=n,max_size=n)
+#        t = text()
+#        return draw(label),draw(x),draw(y),draw(t)
+#    @given(generate_good_LineChart_data())
+#    def test_LineChart(g):
+#        l2 = LineChart()
+#        label,x,y,t = g
+#        l2.add_DataSet(label,x,y)
+#        l2.edit_title(t)
+#        l2.edit_xAxes(t)
+#        l2.edit_yAxes(t,"linear")
+#        assert l2.title["text"]==t
+#        assert l2.xAxes[0]["scaleLabel"]["labelString"] = t
+#        assert l2.yAxes[0]["scaleLabel"]["labelString"] = t
+#    def test_LineChart_exception():
+#        l2 = LineChart
+#
+#class TestBarChart:
+#    @composite
+#    def generate_good_BarChart_data(draw):
+#        n = draw(integers(0,50))
+#        label = text()
+#        x=iterables(one_of(integers(),floats()),min_size=n,max_size=n)
+#        y=iterables(one_of(integers(),floats()),min_size=n,max_size=n)
+#        t = text()
+#        return draw(label),draw(x),draw(y),draw(t)
+#    @given(generate_good_BarChart_data())
+#    def test_BarChart(g):
+#        b2 = BarChart()
+#        label,x,y,t = g
+#        b2.add_DataSet(label,x,y)
+#        b2.edit_title(t)
+#        b2.edit_xAxes(t)
+#        b2.edit_yAxes(t,"linear")
+#        assert b2.title["text"]==t
+#        assert b2.xAxes[0]["scaleLabel"]["labelString"] = t
+#        assert b2.yAxes[0]["scaleLabel"]["labelString"] = t
