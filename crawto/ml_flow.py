399db69d20f0ae6ae1bc2d68aa78610ed49860d4
Crawford <crawford.collins@gmail.com>
2020-02-15 19:18:36 -0600

    more progress replacing the class with a flow

--- crawto/ml_flow.py
+++ crawto/ml_flow.py
@@ -2,6 +2,8 @@ from prefect import task, Flow
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer, MissingIndicator
+from sklearn.preprocessing import LabelEncoder, PowerTransformer
+import numpy as np
 
 @task
 def extract_train_valid_split(input_data, problem, target):
@@ -102,7 +104,7 @@ def fit_missing_indicator(train_data, undefined_features):
 
 
 @task
-def transform_missing_indicator_df(data,undefined_features,missing_indicator):
+def transform_missing_indicator_df(data, undefined_features, missing_indicator):
     x = missing_indicator.transform(data[undefined_features])
     x_labels = ["missing_" + i for i in undefined_features]
     missing_indicator_df = pd.DataFrame(x, columns=x_labels)
@@ -113,6 +115,84 @@ def transform_missing_indicator_df(data,undefined_features,missing_indicator):
     ]
     return missing_indicator_df[columns].replace({True: 1, False: 0})
 
+
+@task
+def fit_numeric_imputer(train_data, numeric_features):
+    numeric_imputer = SimpleImputer(strategy="median", copy=True)
+    numeric_imputer.fit(train_data[numeric_features])
+    return numeric_imputer
+
+
+@task
+def impute_numeric_df(numeric_imputer, data, numeric_features):
+    x = numeric_imputer.transform(data[numeric_features])
+    x_labels = [i + "imputed_" for i in numeric_features]
+    imputed_numeric_df = pd.DataFrame(x, columns=x_labels)
+    return imputed_numeric_df
+
+
+@task
+def fit_yeo_johnson_transformer(train_imputed_numeric_df):
+    yeo_johnson_transformer = PowerTransformer(method="yeo-johnson", copy=True)
+    yeo_johnson_transformer.fit(train_imputed_numeric_df)
+    return yeo_johnson_transformer
+
+
+@task
+def transform_yeo_johnson_transformer(data, yeo_johnson_transformer):
+    yj = yeo_johnson_transformer.transform(data)
+    columns = data.columns.values
+    columsn = [i + "_yj" for i in columns]
+    yj = pd.DataFrame(yj, columns=columns)
+    return yj
+
+
+@task
+def target_transformer(problem, target, data):
+    if problem == "binary classification":
+        return data[target]
+    elif problem == "regression":
+        target_transformer = PowerTransformer(method="yeo-johnson",copy=True)
+        target_transformer.fit(data[target])
+        return target_transformer
+
+@task
+def transform_target(problem, target, data,target_transformer):
+        target_array = yeo_johnson_transformer.fit(
+            np.array(data[target]).reshape(-1, 1)
+        )
+        target_array = pd.DataFrame(target, columns=[target])
+        return target_array
+
+
+@task
+def fit_target_encoder(train_imputed_categorical_df, train_transformed_target):
+    te = TargetEncoder(cols=train_imputed_categorical_df.columns.values)
+    te.fit(X=self.train_imputed_categorical_df, y=self.train_transformed_target)
+    return te
+
+
+@task
+def merge_transformed_data(encoded_df, yeo_johnson_df, missing_df):
+    transformed_data = (
+        encoded_df.merge(yeo_johnson_df, left_index=True, right_index=True)
+        .merge(missing_df, left_index=True, right_index=True)
+        .replace(np.nan, 0)
+    )
+    return transformed_data
+
+
+@task
+def fit_hbos_transformer(train_transformed_data):
+    hbos = HBOS()
+    hbos.fit(train_transformed_data)
+    return hbos_transformer
+
+
+@task
+def transfom_hbos_transformer(data, hbos_transformer):
+    hbos_transformed = hbos_transformer.predict(data)
+    return hbos_t
 
 
 # with Flow("data_cleaning") as flow:
