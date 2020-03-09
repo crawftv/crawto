4f45cee61d0212c24815a69288fa720110d9cc62
Crawford Collins <crawford.collins@topbox.io>
2020-03-09 00:10:13 -0500

    figuring out meta model

--- crawto/meta_model.py
+++ crawto/meta_model.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
+import functools
+
 
 class MetaModel(object):
-
-    def __init__(
-            problem
-    ):
+    def __init__(self, problem):
         self.problem = problem
+        self.models = []
+
+    def add_model_to_meta_model(self, model):
+        self.models.append(model)
+
+    def model(self, model):
+        self.add_model_to_meta_model(model)
+
+class Model(object):
+    def __init__(self, model, problem):
+        self.problem = problem
+        self.model = model
+
+    def fit(self, X, y):
+        return self.model.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+
