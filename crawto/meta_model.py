bb50433c327c7e16e3cc7ae2142c573d3d10f4b4
Crawford Collins <crawford.collins@topbox.io>
2020-03-09 22:30:37 -0500

    reorganized base model creation into a single class

--- crawto/meta_model.py
+++ crawto/meta_model.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
-<<<<<<< HEAD
-import functools
+from sklearn.ensemble import (
+    RandomForestClassifier,
+    RandomForestRegressor,
+    GradientBoostingClassifier,
+    GradientBoostingRegressor,
+)
+from sklearn.tree import(
+
+    DecisionTreeRegressor,
+    DecisionTreeClassifier,
+)
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import (
+    LinearRegression,
+    LogisticRegression,
+    ElasticNet,
+    Ridge,
+    RidgeClassifier,
+)
+from .baseline_model import (
+    BaselineClassificationPrediction,
+    BaselineRegressionPrediction
+    )
 
 
 class MetaModel(object):
@@ -9,10 +30,27 @@ class MetaModel(object):
         self.models = []
 
     def add_model_to_meta_model(self, model):
-        self.models.append(model)
+        self.models.append(model, self.problem)
 
     def model(self, model):
-        self.add_model_to_meta_model(model)
+        self.add_model_to_meta_model(model,)
+    def default_models(self):
+        if self.problem == "regression":
+            self.model(ElasticNet())
+            self.model(LinearRegression())
+            self.model(BaselineRegressionPrediction())
+            self.model(DecisionTreeRegressor())
+            self.model(Ridge())
+            self.model(GradientBoostingRegressor())
+            self.model(RandomForestRegressor())
+            self.model(LogisticRegression())
+        elif self.problem == "classificiation":
+            self.model(BaselineClassificationPrediction())
+            self.model(DecisionTreeClassifier())
+            self.model(LinearSVC())
+            self.model(RandomForestClassifier())
+            self.model(GradientBoostingClassifier())
+            self.model(RidgeClassifier())
 
 class Model(object):
     def __init__(self, model, problem):
@@ -24,14 +62,3 @@ class Model(object):
 
     def predict(self, X):
         return self.model.predict(X)
-
-
-=======
-
-class MetaModel(object):
-
-    def __init__(
-            problem
-    ):
-        self.problem = problem
->>>>>>> deb49cd8da39f2a4768f2cd229f4bb0e6befabe0
