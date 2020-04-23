6c64449a3ee2876facf93dcc03d590e481a6fe66
crawford <crawford.collins@gmail.com>
2020-04-23 18:27:38 -0500

    reorganized tests for flows to be grouped by classificaiton/regressions

--- tests/test_Flows_classification.py
+++ tests/test_Flows_classification.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 
 import pytest
-
 from prefect.engine.executors import DaskExecutor
 from crawto.meta_model import MetaModel, meta_model_flow
 from tinydb import TinyDB
+from prefect import Flow, Parameter, unmapped
+import pandas as pd
+from crawto.ml_flow import data_cleaning_flow
 
 
-def test_meta_model_regression():
-    meta = MetaModel("regression", TinyDB("db.json"), use_default_models=True)
-    models = meta.models
+def test_data_cleaner_end_to_end_classification():
+    input_df = pd.read_csv("data/titanic/train.csv")
+    test = pd.read_csv("data/titanic/test.csv")
     executor = DaskExecutor()
-    meta_model_run = meta_model_flow.run(
-        train_data="transformed_train.df",
-        valid_data="transformed_valid.df",
-        train_target="train_target.df",
-        problem="regression",
-        models=models,
-        tinydb="db.json",
+    data_cleaner = data_cleaning_flow.run(
+        input_data=input_df,
+        problem="binary classification",
+        target="Survived",
+        features="infer",
         executor=executor,
     )
-    assert meta_model_run.message == "All reference tasks succeeded."
+    assert data_cleaner.message == "All reference tasks succeeded."
 
 
 def test_meta_model_classification():
