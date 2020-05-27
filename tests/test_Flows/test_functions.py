5089210bf545a08299c6969de618edcfaf64fee4
Crawford Collins <crawford.collins@gmail.com>
2020-05-27 18:54:16 -0500

    adding tests

--- tests/test_Flows/test_functions.py
+++ tests/test_Flows/test_functions.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from crawto.ml_flow import df_to_sql_schema
+from crawto.ml_flow import df_to_sql_schema,create_sql_data_tables
 import pandas as pd
+from prefect import Parameter, Flow
 
 
 def test_df_to_sql_schema():
@@ -14,3 +15,13 @@ def test_df_to_sql_schema1():
     d = {"col1": [1, 2], "col2": [3, 4]}
     df = pd.DataFrame(data=d)
     assert df_to_sql_schema("test", df["col1"])
+
+def test_create_sql_data_tables():
+    with Flow("test_data_tables") as test:
+        db=Parameter("db")
+        create_sql_data_tables(db)
+
+    result = test.run(
+        db = ":memory:"
+    )
+    assert result.message == "All reference tasks succeeded."
