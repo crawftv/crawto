6aa244a0f1c585f8b364d622ac8ede904e040d4f
crawford <crawford.collins@gmail.com>
2020-05-28 14:23:03 -0500

    cli tool is running

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="crawto-crawftv",
-    version="0.0.1",
+    version="0.0.2",
     author="Crawford Collins",
     url="https://github.com/crawftv/crawto",
     packages=setuptools.find_packages(),
@@ -14,4 +14,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.8",
+    test_suite="tests",
+    entry_points={"console_scripts": ["crawto-ml = crawto.__main__:main",],},
 )
